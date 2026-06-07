2432f12c1c910d85eb5fc954ece92ae46ef6314a
M7Saeed4 <sydfn@gmail.com>
2026-06-07 21:33:09 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/tassk1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tassk1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Basic Stack Navigation',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+// 
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const DetailScreen(),
+              ),
+            );
+          },
+          child: const Text('Go to Details'),
+        ),
+      ),
+    ); 
+  }
+}
+
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detail Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+         
+            Navigator.pop(context);
+          },
+          child: const Text('Back to Home'),
+        ),
+      ),
+    );
+  }
+}
